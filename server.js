bb26bf0a41c4506e645b6b3a271c24f78968ca60
Vo <vob2014721@student.ctu.edu.vn>
2023-02-04 11:14:17 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const config = require("./app/config/index.js");
+
+// Start server
+const port = config.app.port;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
