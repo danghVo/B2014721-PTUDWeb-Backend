8e0c5b605becb3455878e7abad7d94571294fab5
Vo <vob2014721@student.ctu.edu.vn>
2023-02-10 11:35:21 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/utils/mongodb.util.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+
+const config = require("../config/index.js");
+const uri = config.db.uri;
+class MongoDB {
+    static connect = async (uri) => {
+        if(this.client) return this.client;
+        this.client = await MongoClient.connect(uri);
+        return this.client;
+    }
+}
+
+module.exports = MongoDB
